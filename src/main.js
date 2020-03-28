9b78cfb480052cb4c2aa7140e75ad97c3783568e
othmar52 <othmar52@users.noreply.github.com>
2020-03-28 10:11:11 +0100

    [TRYOUT] initial load of json via file protocol

--- src/main.js
+++ src/main.js
@@ -1,12 +1,43 @@
 import Vue from 'vue'
 import App from './App.vue'
+//import VueRx from 'vue-rx'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+//Vue.use(VueRx)
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+
+// due to the requirement of supporting file:// protocol we can't
+// perform an ajax call. but we can load a script during runtime by creating a DOM node...
+let bigDataNode = document.createElement('script')
+bigDataNode.setAttribute('src', './output/bigData.json')
+document.head.appendChild(bigDataNode)
+
+window.initialDataLoadInterval = window.setInterval(
+  function(){
+    if(typeof bigData === "undefined") {
+      // continue loop until we get bigData from external file...
+      return
+    }
+    // now we ar ready to go
+    // destroy the interval and init Vue app
+    clearInterval(window.initialDataLoadInterval)
+    bigDataNode.parentNode.removeChild(bigDataNode);
+
+    new Vue({
+      router,
+      data: {
+        bigData: bigData,
+        deviceFilters: bigData.devices,
+        categoryFilters: bigData.categories,
+        creatorFilters: bigData.creators,
+      },
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+    return
+  },
+  5
+);
+
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+var path = require('path')
+module.exports = {
+  publicPath: './',
+  lintOnSave: false, // TODO remove as soon as tryout phase is finished
+}
