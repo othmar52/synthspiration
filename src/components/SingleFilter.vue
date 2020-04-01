034f75cc42792ec3586e7bea1f8bdc06eb0b7a0e
othmar52 <othmar52@users.noreply.github.com>
2020-04-01 16:07:33 +0200

    [TASK] added colors to device filter buttons

--- src/components/SingleFilter.vue
+++ src/components/SingleFilter.vue
@@ -1,6 +1,9 @@
 <template>
-  <a :class="`btn single__filter single__filter-${stateClass}`" v-on:click="filterClick">
-    <nobr>{{filterLabel}} <span class="amount">({{currentResults}})</span></nobr>
+  <a :class="`btn single__filter single__filter-${stateClass} single__filter-${currentResults} color-${filterColor}`"
+  v-on:click="filterClick">
+    <span>
+    {{filterLabel}}<span class="amount">({{currentResults}})</span>
+    </span>
   </a>
 </template>
 
@@ -8,7 +11,6 @@
 import  { mapGetters } from 'vuex'
 export default {
   name: 'SingleFilter',
-
   data: function () {
     return {
       active: false
@@ -21,7 +23,8 @@ export default {
   computed: {
     ...mapGetters([
       'getAmountForFilter',
-      'getLabelForFilter'
+      'getLabelForFilter',
+      'getColorForFilter'
     ]),
     stateClass() {
       return (this.active === true) ? 'active' : '';
@@ -29,37 +32,66 @@ export default {
     currentResults() {
       return this.getAmountForFilter(this.group, this.label)
     },
-    filterLabel: function() {
+    filterLabel() {
       return this.getLabelForFilter(this.group, this.label)
+    },
+    filterColor() {
+      return this.getColorForFilter(this.group, this.label)
     }
   },
   methods: {
-    filterClick:function(event) {
+    filterClick: function() {
       this.active = this.active ? false : true;
       this.$store.dispatch('setFilter', this)
     }
 
-  },
-  watch: {
-
-  },
-    mounted(){
-        //console.log("SingleFilter", this.label)
-    }
+  }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
 .single__filter {
   padding: 1em;
   flex-basis: 80px;
   flex-grow: 1;
   margin: 3px;
+  font-size: 0.9em;
 }
 
 .btn.single__filter-active {
   background-color: #333;
   color: #BBB;
 }
+
+.single__filter-0 {
+  opacity: 0.3;
+}
+
+.single__filter>span {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.single__filer .amount {
+  display: inline-block;
+  padding-left: 0.4em;
+}
+
+.single__filter {
+  border-bottom: 2px solid #07101b;
+  
+}
+
+/* @see https://www.donaufischer.com/java/farbnamen.htm */
+.color-lawngreen     { border-color: #7CFC00; }
+.color-darkgoldenrod { border-color: #B8860B; }
+.color-grey          { border-color: #808080; }
+.color-teal          { border-color: #008080; }
+.color-firebrick     { border-color: #B22222; }
+.color-darkorange    { border-color: #FF8C00; }
+.color-darkblue      { border-color: #00008B; }
+.color-dodgerblue    { border-color: #1E90FF; }
+
 </style>
